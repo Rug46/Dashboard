744c7f66f94725b0936c6ef285df19fb6da98a2c
Ethan Lane <ethan.lane@influentialsoftware.com>
2019-03-01 11:03:43 +0000

    Backlog Edit Page

--- Dashboard/Controllers/BacklogController.cs
+++ Dashboard/Controllers/BacklogController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Dashboard.Models;
+using Dashboard.Helpers;
 
 namespace Dashboard.Controllers
 {
@@ -11,6 +13,59 @@ namespace Dashboard.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult New()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult New(string Name, string Compilation, string System, int Status, string Progress, bool NowPlaying)
+        {
+            if (!Backlog.New(Name, Compilation, System, Status, Progress, NowPlaying))
+            {
+                TempData["Error"] = "Please fill in the required fields (marked with a *)";
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var BacklogEntry = Backlog.Get(id);
+
+            if (BacklogEntry == null)
+            {
+                TempData["Error"] = "This entry doesn't exist";
+            }
+            else
+            {
+                TempData["Backlog"] = BacklogEntry;
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int Id, string Name, string Compilation, string System, int Status, string Progress, bool NowPlaying)
+        {
+            if (!Backlog.Edit(Id, Name, Compilation, System, Status, Progress, NowPlaying))
+            {
+                TempData["Error"] = "Please fill in the required fields (marked with a *)";
+                return RedirectToAction("Index");
+            }
+
+            TempData["Success"] = "Successfully Edited";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Backlog.Delete(Id);
+            return RedirectToAction("Index");
         }
     }
 }
--- Dashboard/Helpers/Backlog.cs
+++ Dashboard/Helpers/Backlog.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Dashboard.Data;
+using Dashboard.Models;
 
 namespace Dashboard.Helpers
 {
@@ -21,10 +22,8 @@ namespace Dashboard.Helpers
 
                 for (int i = 0; i < records.Count; i++)
                 {
-                    var val = list
-                        .Where(s => s.Contains(records.ElementAt(i).System));
 
-                    if (val != null)
+                    if (!list.Contains(records.ElementAt(i).System))
                     {
                         list.Add(records.ElementAt(i).System);
                     }
@@ -76,6 +75,183 @@ namespace Dashboard.Helpers
                 }
 
                 return count;
+            }
+        }
+
+        public static double GetFromStatusPercentage(int status)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Backlog
+                    .OrderBy(bm => bm.Id)
+                    .ToList();
+
+                int count = 0;
+                int total = records.Count;
+
+                for (int i = 0; i < records.Count; i++)
+                {
+                    if (records.ElementAt(i).Status == status)
+                    {
+                        count++;
+                    }
+                }
+
+                if (total == 0)
+                {
+                    return 0;
+                }
+
+                return (double) count / (double) total * (double) 100;
+            }
+        }
+
+        public static int GetFromStatusPercentage(int status, string system)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Backlog
+                    .Where(bm => bm.System == system)
+                    .OrderBy(bm => bm.Id)
+                    .ToList();
+
+                var count = 0;
+                var total = records.Count;
+
+                for (int i = 0; i < records.Count; i++)
+                {
+                    if (records.ElementAt(i).Status == status)
+                    {
+                        count++;
+                    }
+                }
+
+                if (total == 0)
+                {
+                    return 0;
+                }
+
+                return (count / total) * 100;
+            }
+        }
+
+        public static bool New(string Name, string Compilation, string System, int Status, string Progress, bool NowPlaying)
+        {
+            using (var db = new Database())
+            {
+                var NowPlayingInt = 0;
+
+                if (NowPlaying) NowPlayingInt = 1;
+
+                if (Name == null || System == null)
+                {
+                    return false;
+                }
+
+                var single = new BacklogModel
+                {
+                    Name = Name,
+                    Compilation = Compilation,
+                    System = System,
+                    Status = Status,
+                    Progress = Progress,
+                    NowPlaying = NowPlayingInt
+                };
+
+                db.Backlog.Add(single);
+                db.SaveChanges();
+
+                return true;
+            }
+        }
+
+        public static int GetSystemTotal(string system)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Backlog
+                    .Where(bm => bm.System == system)
+                    .OrderBy(bm => bm.Id)
+                    .ToList();
+
+                return records.Count;
+            }
+        }
+
+        public static int GetTotal()
+        {
+            using (var db = new Database())
+            {
+                var records = db.Backlog
+                    .OrderBy(bm => bm.Id)
+                    .ToList();
+
+                return records.Count;
+            }
+        }
+
+        public static BacklogModel Get(int id)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Backlog
+                    .Where(bm => bm.Id == id)
+                    .ToList();
+
+                if (records.Count != 1)
+                {
+                    return null;
+                }
+
+                return records.ElementAt(0);
+            }
+        }
+
+        public static bool Edit(int id, string Name, string Compilation, string System, int Status, string Progress, bool NowPlaying)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Backlog
+                    .Where(bm => bm.Id == id)
+                    .ToList();
+
+                var NowPlayingInt = 0;
+
+                if (NowPlaying) NowPlayingInt = 1;
+
+                if (records.Count != 1)
+                {
+                    return false;
+                }
+
+                if (Name == null || System == null)
+                {
+                    return false;
+                }
+
+                records.ElementAt(0).Name = Name;
+                records.ElementAt(0).Compilation = Compilation;
+                records.ElementAt(0).System = System;
+                records.ElementAt(0).Status = Status;
+                records.ElementAt(0).Progress = Progress;
+                records.ElementAt(0).NowPlaying = NowPlayingInt;
+
+                db.SaveChanges();
+
+                return true;
+            }
+        }
+
+        public static void Delete(int id)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Backlog
+                    .Where(bm => bm.Id == id)
+                    .ToList();
+
+                db.Backlog.Remove(records.ElementAt(0));
+                db.SaveChanges();
             }
         }
     }
