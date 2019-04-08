1f7faa94cfc40460b0d241a0cdba2505a35ba1b9
Ethan Lane <ethan.lane@influentialsoftware.com>
2019-04-08 14:19:43 +0100

    Child account basics

---
+++ Dashboard/Controllers/ParentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Dashboard.Controllers
+{
+    [Authorize]
+    public class ParentController : Controller
+    {
+        public IActionResult ChildAccounts()
+        {
+            if (!Helpers.Account.IsParent(Helpers.Account.GetUserId(User.Identity.Name)))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewChild()
+        {
+            if (!Helpers.Account.IsParent(Helpers.Account.GetUserId(User.Identity.Name)))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewChild(string Username, string Password, string Email, int ParentAccountId)
+        {
+            if (!Helpers.Account.IsParent(Helpers.Account.GetUserId(User.Identity.Name)))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            Helpers.Account.RegisterChild(Username, Password, Email, ParentAccountId);
+
+            return RedirectToAction("ChildAccounts");
+        }
+    }
+}
--- Dashboard/Helpers/Account.cs
+++ Dashboard/Helpers/Account.cs
@@ -47,6 +47,45 @@ namespace Dashboard.Helpers
             }
         }
 
+        public static bool RegisterChild(string username, string password, string email, int parentAccountId)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Users
+                    .Where(u => u.Username == username)
+                    .ToList();
+
+                if (records.Count != 0)
+                {
+                    return false;
+                }
+
+                if (username == "" || username == null)
+                {
+                    return false;
+                }
+
+                if (password == null || password.Length < 6)
+                {
+                    return false;
+                }
+
+                UserModel user = new UserModel
+                {
+                    Username = username,
+                    Password = Passwords.Hash(password),
+                    Email = email,
+                    Admin = 0,
+                    ParentAccountId = parentAccountId
+                };
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                return true;
+            }
+        }
+
         public static int GetUserId(string username)
         {
             using (var db = new Database())
@@ -61,6 +100,39 @@ namespace Dashboard.Helpers
                 }
 
                 return records.ElementAt(0).Id;
+            }
+        }
+
+        public static bool IsParent(int UserId)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Users
+                    .Where(um => um.Id == UserId)
+                    .OrderBy(um => um.Id)
+                    .ToList();
+
+                if (records.ElementAt(0).Admin == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public static string GetEmail(int UserId)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Users
+                    .Where(um => um.Id == UserId)
+                    .OrderBy(um => um.Id)
+                    .ToList();
+
+                return records.ElementAt(0).Email;
             }
         }
     }
