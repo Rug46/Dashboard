0c9bf2eb0542936ce1aa4597ecd61b3fbd1d9c3c
Ethan Lane <ethan.lane@influentialsoftware.com>
2019-04-09 11:45:21 +0100

    Budget parent authorisation

--- Dashboard/Controllers/InsightController.cs
+++ Dashboard/Controllers/InsightController.cs
@@ -11,6 +11,8 @@ namespace Dashboard.Controllers
     [Authorize]
     public class InsightController : Controller
     {
+        private static int _monthHours, _monthMinutes, _dayHours, _dayMinutes;
+
         public IActionResult Index()
         {
             return RedirectToAction("Budget");
@@ -35,9 +37,24 @@ namespace Dashboard.Controllers
                 var dayTotalMinutes = (dayHours * 60) + dayMinutes;
 
                 var records = db.UserBudgets
-                    .Where(bm => bm.UserId == Dashboard.Helpers.Account.GetUserId(User.Identity.Name))
+                    .Where(bm => bm.UserId == Helpers.Account.GetUserId(User.Identity.Name))
                     .OrderBy(bm => bm.id)
                     .ToList();
+
+                var userRecords = db.Users
+                    .Where(um => um.Id == Helpers.Account.GetUserId(User.Identity.Name))
+                    .OrderBy(um => um.Id)
+                    .ToList();
+
+                if (userRecords.ElementAt(0).Admin == 0) // Not an admin
+                {
+                    _monthHours = monthHours;
+                    _monthMinutes = monthMinutes;
+                    _dayHours = dayHours;
+                    _dayMinutes = dayMinutes;
+
+                    return RedirectToAction("Authorise");
+                }
 
                 records.ElementAt(0).Monthly = monthTotalMinutes;
                 records.ElementAt(0).Daily = dayTotalMinutes;
@@ -48,25 +65,51 @@ namespace Dashboard.Controllers
             }
         }
 
+        [HttpGet]
+        public IActionResult Authorise()
+        {
+            return View();
+        }
+
         [HttpPost]
-        public string SetBudgetAmount(int monthHours, int monthMinutes, int dayHours, int dayMinutes)
+        public IActionResult Authorise(string password)
         {
             using (var db = new Database())
             {
-                var monthTotalMinutes = (monthHours * 60) + monthMinutes;
-                var dayTotalMinutes = (dayHours * 60) + dayMinutes;
+                var userRecords = db.Users
+                    .Where(um => um.Id == Helpers.Account.GetUserId(User.Identity.Name))
+                    .OrderBy(um => um.Id)
+                    .ToList();
 
-                var records = db.UserBudgets
+                var parentRecords = db.Users
+                    .Where(um => um.Id == userRecords.ElementAt(0).ParentAccountId)
+                    .OrderBy(um => um.Id)
+                    .ToList();
+
+                if (password == null || password == "")
+                {
+                    return View();
+                }
+
+                if (Helpers.Passwords.PasswordCorrect(parentRecords.ElementAt(0).Username, password))
+                {
+                    var records = db.UserBudgets
                     .Where(bm => bm.UserId == Helpers.Account.GetUserId(User.Identity.Name))
                     .OrderBy(bm => bm.id)
                     .ToList();
 
-                records.ElementAt(0).Monthly = monthTotalMinutes;
-                records.ElementAt(0).Daily = dayTotalMinutes;
-                    
-                db.SaveChanges();
+                    var monthTotalMinutes = (_monthHours * 60) + _monthMinutes;
+                    var dayTotalMinutes = (_dayHours * 60) + _dayMinutes;
 
-                return "Month: " + monthTotalMinutes + ", Day: " + dayTotalMinutes;
+                    records.ElementAt(0).Monthly = monthTotalMinutes;
+                    records.ElementAt(0).Daily = dayTotalMinutes;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Budget");
+                }
+
+                return View();
             }
         }
     }
