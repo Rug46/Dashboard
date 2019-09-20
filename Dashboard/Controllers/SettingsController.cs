abcca8c35d5b3bd1afdf8727466d6eee5b265c1e
Ethan Lane <ethan.lane@influentialsoftware.com>
2019-09-20 15:47:25 +0100

    Change password

--- Dashboard/Controllers/SettingsController.cs
+++ Dashboard/Controllers/SettingsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Dashboard.Helpers;
+using Dashboard.Data;
 
 namespace Dashboard.Controllers
 {
@@ -54,6 +55,63 @@ namespace Dashboard.Controllers
             TempData["private"] = UserSetting.GetSettingValueOrDefault(currentUser, Setting.GetSettingId("private"));
 
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ChangePassword(string currentPassword, string newPassword, string repeatPassword)
+        {
+            using (var db = new Database())
+            {
+                var records = db.Users
+                    .Where(um => um.Username == User.Identity.Name)
+                    .ToList();
+
+                if (currentPassword != null)
+                {
+                    if (Passwords.PasswordCorrect(User.Identity.Name, currentPassword))
+                    {
+                        if (newPassword == repeatPassword)
+                        {
+                            if (newPassword == null || newPassword.Length < 6)
+                            {
+                                TempData["Error"] = "Password must be at least 6 characters long";
+                                return View();
+                            }
+                            else
+                            {
+                                var newPasswordHash = Passwords.Hash(newPassword);
+
+                                records.ElementAt(0).Password = newPasswordHash;
+                                db.SaveChanges();
+
+                                TempData["Success"] = "Password changed successfully";
+                                return View();
+                            }
+                        }
+                        else
+                        {
+                            TempData["Error"] = "Passwords do not match";
+                            return View();
+                        }
+                    }
+                    else
+                    {
+                        TempData["Error"] = "Current password is incorrect";
+                        return View();
+                    }
+                }
+                else
+                {
+                    TempData["Error"] = "Current password can't be empty";
+                    return View();
+                }
+            }
         }
     }
 }
